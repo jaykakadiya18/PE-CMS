eda57dda01598392f379b2f13d85715b636e326f
Jay Kakadiya <jaykakadiya2014@gmail.com>
2025-05-30 14:43:31 +0530

    Update news.ts

--- schemaTypes/news.ts
+++ schemaTypes/news.ts
@@ -3,18 +3,14 @@ export default {
   title: 'News',
   type: 'document',
   fields: [
-    { name: 'title', title: 'Title', type: 'string', validation: (Rule:any) => Rule.required() },
+    { name: 'title', title: 'Title', type: 'string', validation: (Rule: any) => Rule.required() },
     { name: 'slug', title: 'Slug', type: 'slug', options: { source: 'title', maxLength: 96 } },
     { name: 'author', title: 'Author Name', type: 'string' },
     { name: 'publishedDate', title: 'Published Date', type: 'datetime' },
     { name: 'image', title: 'Image', type: 'image', options: { hotspot: true } },
     { name: 'description', title: 'Description', type: 'text' },
     { name: 'body', title: 'Body', type: 'blockContent' },
-    { 
-      name: 'category', 
-      title: 'Category', 
-      type: 'string', 
-      options: { list: ['Global Market', 'Technology'], layout: 'dropdown' } 
-    }
+    { name: 'category', title: 'Category', type: 'string' }, // Changed to plain text
+    { name: 'label', title: 'Label', type: 'text' } // New label field
   ]
 };
