5aac83274187295e5a26eb1399111b81b1a68b83
Jay Kakadiya <jaykakadiya2014@gmail.com>
2025-06-20 17:03:23 +0530

    Update index.ts

--- schemaTypes/shared/index.ts
+++ schemaTypes/shared/index.ts
@@ -2,6 +2,7 @@ import { blockContent } from './blockContent'
 import companyMedia from './media'
 import workflow from './workflow'
 import assetUpload from './assetUpload'
+import companyWorkflow from './companyWorkflow'
 
 export { blockContent, companyMedia, workflow, assetUpload }
 export const sharedSchemas = [blockContent, companyMedia, workflow, assetUpload]
